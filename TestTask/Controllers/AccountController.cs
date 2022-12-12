e6f021e83c7cc842b5ebfa6d719ae4e57db2376b
likviq <p3tro.pavlyk@gmail.com>
2022-12-12 19:59:52 +0200

    implement authorization

---
+++ TestTask/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using TestTask.Config;
+using TestTask.Domain;
+using TestTask.Models;
+using TestTask.Views;
+
+namespace TestTask.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly testsDBContext _context;
+        public AccountController(testsDBContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("/token")]
+        public IActionResult Token([FromBody] UserCredentials user)
+        {
+            var identity = GetIdentity(user.Username, user.Password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                token = encodedJwt
+            };
+
+            return Json(response);
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Username)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+            return null;
+        }
+    }
+}
